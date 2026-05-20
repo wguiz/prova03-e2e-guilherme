f9acf39c8b36753fb8b5935620b09997b64196db
Guilherme Zapelini Da Rosa Schwan <guilherme.201923@alunosatc.edu.br>
2026-05-20 19:55:23 -0300

    .

--- src/scenarios/tests/checkboxes.spec.ts
+++ src/scenarios/tests/checkboxes.spec.ts
@@ -5,14 +5,12 @@ test('checkbox can be selected and unselected', async ({ page }) => {
 
   const checkboxes = page.locator('#checkboxes input');
 
-  // First checkbox is initially unchecked
   await expect(checkboxes.first()).not.toBeChecked();
 
-  // Click to check it
+
   await checkboxes.first().check();
   await expect(checkboxes.first()).toBeChecked();
 
-  // Toggle back
   await checkboxes.first().uncheck();
   await expect(checkboxes.first()).not.toBeChecked();
 });
